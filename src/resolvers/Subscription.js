e070e108e0ff00cd579622dbf39916e5ab6fa768
James Taylor <j.taylor@kainos.com>
2019-09-16 17:47:54 +0100

    Add voting for posts mechanism

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -2,6 +2,10 @@ const newLinkSubscribe = (parent, args, context, info) => {
   return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node()
 }
 
+const newVoteSubscribe = (parent, args, context, info) => {
+  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
+}
+
 const newLink = {
   subscribe: newLinkSubscribe,
   resolve: payload => {
@@ -9,6 +13,14 @@ const newLink = {
   }
 }
 
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  }
+}
+
 module.exports = {
-  newLink
+  newLink,
+  newVote
 }
